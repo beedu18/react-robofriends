750de450d92a4c2d13290be4005b32268ce6e639
unknown <Administrator@DULITLAPTOP07.oilindia.in>
2021-09-28 22:56:46 +0530

    first component with props

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Hello extends Component {
+    render() {
+        return (
+            <div className="f1 tc">
+                <h1>Hello World</h1>
+                <p>Welcome {this.props.greetings}</p>
+            </div>
+        );
+    }
+}
+
+export default Hello;
