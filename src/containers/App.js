1aa441846acde9968ba04833d42584b7b232dd19
beedu18 <b1n4y4k@gmail.com>
2021-10-03 01:01:49 +0530

    App now uses react hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,73 +1,60 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import ScrollBox from "../components/ScrollBox";
 import Error from "../components/Error";
 import "./App.css";
 
-class App extends React.Component {
-    constructor() {
-        console.log("constructor");
-        super();
-        this.state = {
-            robots: [],
-            searchField: ''
-        }
-    }
+function App() {
 
-    // functionName = (...) => {}
-    // use this syntax for custom functions to avoid errors while using 'this' within the functions
-    onSearchChange = (event) => {
-        // console.log(event.target.value);
-        
-        // set new state from search text
-        this.setState({searchField: event.target.value});
+    const [robots, setRobots] = useState([]);
+    const [searchField, setSearchField] = useState('');
+    
+    const onSearchChange = (event) => {
+        //this.setState({searchField: event.target.value});
+        setSearchField(event.target.value); //using hooks
     }
     
-    // react component lifecycle
-    // https://reactjs.org/docs/react-component.html#mounting
-    componentDidMount() {
-        console.log("componentDidMount");
+    // useEffect() is called every time the variables in optional parameter change
+    // all variables should be inside an array
+    // mimics componentDidMount() when optional parameter is '[]' 
+    useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/users")
             .then(response => response.json())
-            .then(users => this.setState({robots: users}));
-    }
+            .then(users => setRobots(users));
+    }, []);
+
+
+    //console.log(searchField);
+    const filteredRobots = robots.filter(
+        robot => robot.name
+            .toLowerCase()
+            .includes(searchField.toLowerCase())
+    );  
 
     // filter robots based on value in search field
-    render () {
-        console.log("render");
-
-        const { robots, searchField } = this.state;
-
-        const filteredRobots = this.state.robots.filter(
-            robot => robot.name
-                .toLowerCase()
-                .includes(searchField.toLowerCase())
-        );   
-
-        if(robots.length == 0){
-            // inform user
-            return (
-                <div>
-                    <h1 className="tc">loading...</h1>
-                </div>
-            )
-        } else {
-            // article on map(), reduce(), filter()
-            // https://medium.com/poka-techblog/simplify-your-javascript-use-map-reduce-and-filter-bd02c593cc2d
-            return (
-                <div className="tc">
-                    <h1 className="mb3">Robofriends</h1>
-                    <SearchBox searchChange = {this.onSearchChange}/>
-                    <ScrollBox>
-                        <Error>
-                             <CardList robots = {filteredRobots} />
-                        </Error>
-                    </ScrollBox>
-                    
-                </div>
-            );
-        }
+    if(robots.length === 0){
+        // inform user
+        return (
+            <div>
+                <h1 className="tc">loading...</h1>
+            </div>
+        );
+    }
+    else {
+        // article on map(), reduce(), filter()
+        // https://medium.com/poka-techblog/simplify-your-javascript-use-map-reduce-and-filter-bd02c593cc2d
+        return (
+            <div className="tc">
+                <h1 className="mb3">Robofriends</h1>
+                <SearchBox searchChange = {onSearchChange}/>
+                <ScrollBox>
+                    <Error>
+                         <CardList robots = {filteredRobots} />
+                    </Error>
+                </ScrollBox>
+            </div>
+        );
     }
 }
 
